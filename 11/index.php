76da0180e55d5096915069b3ef3198118e546c7d
Jimmy419 <suifeng419@126.com>
2018-02-26 21:21:47 +0800

    阿道夫

--- 11/index.php
+++ 11/index.php
@@ -4,12 +4,18 @@
 
 	//获取当前年
 	$year=$_GET['y']?$_GET['y']:date('Y');
+	echo $year;
+	echo "<hr>";
 
 	//获取当前月
-	$month=$_GET['m']?$_GET['m']:date('M');
+	$month=$_GET['m']?$_GET['m']:date('m');
+	echo $month;
+	echo "<hr>";
 
-	//获取当前日
+	//获取当前周几
 	$day=$_GET['d']?$_GET['d']:date('D');
+	echo $day;
+	echo "<hr>";
 
 	//获取当前月的天数
 	$days=date('t',strtotime("{$year}-{$month}-1"));
@@ -17,24 +23,33 @@
 	//当前一号是周几
 	$week=date('w',strtotime("{$year}-{$month}-1"));
 
-	echo "<table>";
+	$preyear=($month==1)?($year-1):$year;
+	$premonth=($month==1)?12:($month-1);
+	$nextyear=($month==12)?($year+1):$year;
+	$nextmonth=($month==12)?1:($month+1);
+
+	echo "<div>";
+	echo "<table style='width:100%'>";
 		echo "<thead>";
 		echo "<tr>";
 		echo "<th style='border:1px solid red;'>周日</th><th style='border:1px solid red;'>周一</th><th style='border:1px solid red;'>周二</th><th style='border:1px solid red;'>周三</th><th style='border:1px solid red;'>周四</th><th style='border:1px solid red;'>周五</th><th style='border:1px solid red;'>周六</th>";
 		echo "</tr>";
 		echo "</thead>";
 		echo "<tbody>";
-			for ($j=1; $j <= $days;) {
+			for ($j=1; $j <= ($days+$week);) {
 				echo "<tr>"; 
 				for ($i=1; $i <= 7 ; $i++) { 
-					echo "<td style='border:1px solid red;'>{$j}</td>";
+					if(($j-$week > $days) || ($j-$week<=0)){
+						echo "<td style='border:1px solid red;'></td>";
+					}else{
+						echo "<td style='border:1px solid red;'>".($j-$week)."</td>";
+					}
 					$j++;
 				}
-				
 				echo "</tr>";
 			}
-
 		echo "</tbody>";
 	echo "</table>";
-
+	echo "<div style='text-align:center;'><a href='index.php?y={$preyear}&m={$premonth}'>上一月</a> | <a href='index.php?y={$nextyear}&m={$nextmonth}'>下一月</a></div>";
+	echo "</div>";
 ?>
