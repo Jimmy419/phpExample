13b93c5c5dbf39bf9599bc27804498ed419c4a78
Jimmy419 <suifeng419@126.com>
2018-03-13 22:11:45 +0800

    555

---
+++ 23/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<div>用户信息表</div>
+<table>
+	<thead>
+		<th>姓名</th>
+		<th>密码</th>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+<div><a href="add.php">增加用户</a></div>
+</body>
+</html>
