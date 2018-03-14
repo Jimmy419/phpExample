823cc11873671c627c7e3e2db0e67c5c2635df2f
Jimmy419 <suifeng419@126.com>
2018-03-14 22:49:49 +0800

    大树

--- 23/index.php
+++ 23/index.php
@@ -1,3 +1,15 @@
+<?php
+
+$conn=mysql_connect("localhost","root","123456");
+
+mysql_select_db("test");
+
+mysql_query("set names utf8");
+
+$sql="select * from user order by id limit 0,5";
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,6 +30,6 @@
 		</tr>
 	</tbody>
 </table>
-<div><a href="add.php">增加用户</a></div>
+<div><a href="index.php?page=<?php echo {$page}?>">上一页</a>|<a href="index.php?page=<?php echo {$page} ?>"></a></div>
 </body>
 </html>
